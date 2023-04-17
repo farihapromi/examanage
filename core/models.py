dac6576500c962e1e2bf270aa1bb9b1894ee97b7
nazma98 <nazmasarker1311@gmail.com>
2023-04-17 11:59:02 +0600

    Add changes to model

--- core/models.py
+++ core/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 # Create your models here.
 
+class ExamSystem(models.Model):
+    semester = models.CharField(max_length=10, blank=True)
+    year = models.CharField(max_length=10)
+
 class Notice(models.Model):
     #name = models.CharField(max_length=100)
     memorial_no = models.CharField(max_length=100)
     semester = models.CharField(max_length=10)
     year = models.CharField(max_length=10)
-    eng_year = models.CharField(max_length=4)
+    exam_year = models.CharField(max_length=4)
     date = models.DateField(auto_now_add=True)
     #notice_generator = models.ForeignKey(Staff, on_delete=models.CASCADE)
 
@@ -19,6 +23,13 @@ class Notice(models.Model):
 
 class NoticeQuesMod(models.Model):
     #staff detail name, dept, university, address
-  
+    date = models.DateField()
+    day = models.CharField(max_length=20)
+    time = models.CharField(max_length=20)
+    exam_system = models.ForeignKey(ExamSystem, on_delete=models.CASCADE)
+    #exam_committee_member = 
+
+
+
     
     
